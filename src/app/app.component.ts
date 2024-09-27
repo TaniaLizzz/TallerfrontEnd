91fc38163f4b6a98ae28e595e0d9ba51f7d37dbc
TaniaLizz <tlizethts13@gmail.com>
2024-09-27 17:56:34 -0500

    Segundo commit: Modificado vista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']  // Cambié styleUrl a styleUrls
 })
 export class AppComponent {
   title = 'frontEnd';
+  isHomePage: boolean = false;
+
+  constructor(private router: Router) {
+    // Filtramos los eventos de navegación
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      // Comprobamos si estamos en la ruta de inicio
+      this.isHomePage = event.url === '/';
+    });
+  }
 }
